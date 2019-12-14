ef757ab5ed49e7673977bd29ae8550fb5d13f5b4
mxl <1113943731@qq.com>
2019-12-14 10:11:04 +0800

    图片目录“ git commit -m  图片目录“

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -73,17 +73,17 @@ class WeixinController extends Controller
         $userInfo=json_decode($yonghu,true);
         //打印用户信息
 //        dd($userInfo);
-       if($xml_obj->MsgType=='event') {
+        $openid = $xml_obj->FromUserName;   //获取用户的openid
+        if($xml_obj->MsgType=='event') {
            $event = $xml_obj->Event;  //获取事件7类型 是不是关注
            if ($event == 'subscribe') {
-               $oppenid = $xml_obj->FromUserName;   //获取用户的oppenid
                $user_data = [
-                   'openid' => $oppenid,
+                   'openid' => $openid,
                    'sub_time' => $xml_obj->CreateTime,
                    'nickname' =>$userInfo['nickname'],
                    'sex' =>$userInfo['sex']
                ];
-               $u = WxUserModel::where(['openid' => $oppenid])->first();
+               $u = WxUserModel::where(['openid' => $openid])->first();
                if ($u) {
                    $this->huifu($xml_obj,3,$userInfo['nickname']);
                } else {
@@ -92,8 +92,16 @@ class WeixinController extends Controller
                    $this->huifu($xml_obj,2,$userInfo['nickname']);
                }
            }
+       }elseif($xml_obj->MsgType=='image'){ //图片
+            $media_id=$xml_obj->MediaId;
+            $openid=$xml_obj->FromUserName;
+//            dd($media_id);die;
+            $res=$this->picture($media_id,$openid);
+            if($res){
+                $this->huifu($xml_obj,4,$userInfo['nickname']);
+            }
+        }
 
-       }
 
         $msg_type = $xml_obj->MsgType;
         if ($msg_type == 'text') {
@@ -105,8 +113,8 @@ class WeixinController extends Controller
     /**
      *获取用户基本信息
      */
-    public function getUserInfo($access_token,$oppenid){
-        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$oppenid.'&lang=zh_CN';
+    public function getUserInfo($access_token,$openid){
+        $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         //发送网络请求
         $json_str = file_get_contents($url);
         $log_file = 'wx.user.log';
@@ -116,7 +124,7 @@ class WeixinController extends Controller
      //给用户发送消息
     public  function  huifu($xml_obj,$code,$nickname){
         $time = time();
-        $touser = $xml_obj->FromUserName;  //接受用户的oppenid
+        $touser = $xml_obj->FromUserName;  //接受用户的openid
         $fromuser = $xml_obj->ToUserName;   //开发者公众号的id
 
         if($code==1){
@@ -125,6 +133,10 @@ class WeixinController extends Controller
             $content ="您好 ". $nickname ." 现在北京时间".date('Y-m-d H:i:s') . "   " . "欢迎关注";
         }elseif($code==3){
             $content = "您好 ". $nickname ." 现在北京时间".date('Y-m-d H:i:s') . "   " . "欢迎回来";
+        }elseif($code==3){
+            $content = "您好 ". $nickname ." 现在北京时间".date('Y-m-d H:i:s') . "   " . "欢迎回来";
+        }elseif($code==4){
+            $content = "您好 ". $nickname ." 现在北京时间".date('Y-m-d H:i:s') . "   " . "下载成功";
         }
 
         $response_text = '<xml>
@@ -139,16 +151,17 @@ class WeixinController extends Controller
     }
 
     //微信下载图片
-    public  function  picture(){
-        $media_id="lnUIXkKd61X0W71rXf3xPeQFWWiA-qae-JZO4yF93xKEOkb-jQUQMTP32PiM2niP";
+    public  function  picture($media_id,$openid){
         $access_token=$this->GetAccessToken();
         $url="https://api.weixin.qq.com/cgi-bin/media/get?access_token=$access_token&media_id=$media_id";
          //下载图片
         $img= file_get_contents($url);
+        $time=date('Ymd');
         //保存图片
-        file_put_contents('cat.jpg',$img);
+        file_put_contents('ziliao/'.$time.'/'.$openid.'/cat2.jpg',$img);
+//        file_put_contents('123/cat2.jpg',$img);
 
-
+     return "下载成功";
     }
 
 }
